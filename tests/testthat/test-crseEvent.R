5f2b7e6f641ce9400c5ef91b7109964b2476cfaa
Siegfried Köstlmeier <siegfried.koestlmeier@gmail.com>
2019-08-07 11:28:07 +0200

    Fixed bug in test-case

--- tests/testthat/test-crseEvent.R
+++ tests/testthat/test-crseEvent.R
@@ -1,7 +1,7 @@
 context('functions')
 
 test_that('crseEvent', {
-  tmp <- crseEvent(demo_share_repurchases, abnr = "ars", cluster1 = "date")
+  tmp <- crseEvent(demo_share_repurchases, abnr = "ars", cluster1 = "date", cluster2 = "code")
 
   expect_error(crseEvent(abnr = "ars", cluster1 = "date"))
   expect_error(crseEvent(data = demo_share_repurchases, abnr = "ars", cluster1 = "date", na.rm = "FALSE"))
@@ -9,7 +9,7 @@ test_that('crseEvent', {
   
   test <- as.data.frame(matrix(c(NA, NA, 1, 1), ncol = 2))
   colnames(test) <- c("first", "second")
-  expect_error(crseEvent(data = test, abnr = "first", cluster1 = "date", na.rm = TRUE))
+  expect_error(crseEvent(data = test, abnr = "first", cluster1 = "second", na.rm = TRUE))
   
   expect_warning(crseEvent(data = demo_share_repurchases, abnr = "ars", cluster1 = "date", na.rm = FALSE, na.replace = 0))
   
@@ -22,7 +22,8 @@ test_that('crseEvent', {
   expect_equal(tmp$N, 2340)
   expect_equal(tmp$unique.cl1, 145)
   expect_equal(tmp$cluster1, "date")
-  expect_equal(tmp$tcl2, NA)
+  expect_equal(tmp$cluster2, "code")
+  expect_equal(tmp$tcl2, -0.2037347, tolerance=1e-7)
   expect_equal(tmp$var.cl1[1,1], 0.001883136, tolerance=1e-7)
   expect_equal(tmp$mean.abnormal.ret, -0.006964425, tolerance=1e-7)
 
@@ -31,4 +32,5 @@ test_that('crseEvent', {
   expect_gte(tmp$p.val.one.clustered,expected = 0)
   expect_lte(tmp$p.val.nonclustered, expected = 1)
   expect_lte(tmp$p.val.one.clustered,expected = 1)
+
 })
